9244065905a8b85746edc618e27dc3296c6623aa
Craig Knobaluch <craigknoblauch@gmail.com>
2025-01-30 19:26:42 -0500

    rocket launch component

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,14 @@
 import { OrbitControls } from "@react-three/drei"
 import { VanguardModel } from "src/components/models/VanguardModel"
 import { AsuModel } from "src/components/models/AsuModel"
+import { RocketLaunchModel } from "src/components/models/RocketLaunchModel"
 
 export const Experience = () => {
     return <>
         <OrbitControls />
         <ambientLight intensity={2} />
         {/* <VanguardModel /> */}
-        <AsuModel />
+        {/* <AsuModel /> */}
+        <RocketLaunchModel />
     </>
 }
