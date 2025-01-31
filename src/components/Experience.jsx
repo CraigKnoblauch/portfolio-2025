c51665cf4ad9a3451ce407cce7b8035db9ec2eb6
Craig Knobaluch <craigknoblauch@gmail.com>
2025-01-30 20:02:30 -0500

    rest of the models; gltfjsx approach doesn't work for materials. Need matcap approach from portfolio-2024

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,6 +2,13 @@ import { OrbitControls } from "@react-three/drei"
 import { VanguardModel } from "src/components/models/VanguardModel"
 import { AsuModel } from "src/components/models/AsuModel"
 import { RocketLaunchModel } from "src/components/models/RocketLaunchModel"
+import { NrlModel } from "./models/NrlModel"
+import { PhoenixLogoModel } from "./models/PhoenixLogoModel"
+import { CubesatModel } from "./models/CubesatModel"
+import { GithubModel } from "./models/GithubModel"
+import { LinkedinModel } from "./models/LinkedinModel"
+import { MailboxModel } from "./models/MailboxModel"
+import { RabbitHoleModel } from "./models/RabbitHoleModel"
 
 export const Experience = () => {
     return <>
@@ -9,6 +16,13 @@ export const Experience = () => {
         <ambientLight intensity={2} />
         {/* <VanguardModel /> */}
         {/* <AsuModel /> */}
-        <RocketLaunchModel />
+        {/* <RocketLaunchModel /> */}
+        {/* <NrlModel /> */}
+        {/* <PhoenixLogoModel /> */}
+        {/* <CubesatModel /> */}
+        {/* <GithubModel /> */}
+        {/* <LinkedinModel /> */}
+        {/* <MailboxModel /> */}
+        <RabbitHoleModel />
     </>
 }
