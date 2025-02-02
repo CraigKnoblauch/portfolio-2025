5f5a0a1a2c3adf482da6aca6f29afd3b3d972e80
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-01 21:54:42 -0500

    work on information card; animations for transition works. Will need more formatting, tying it to a different position in scene for mobile and desktop

--- src/components/InformationCard.jsx
+++ src/components/InformationCard.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 /**
  * Displays:
  *  - Title
@@ -7,8 +8,36 @@
  *  - Dropdown content including a write up of experiences
  */
 
-export const InformationCard = () => {
-    return <>
-        <button onClick={() => alert("hi")}>BUTTON</button>
-    </>
-}
+export const InformationCard = ({ title, startDate, endDate, experiences }) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        
+        <div className="bg-white opacity-75 p-4 rounded-lg">
+            {/* header contains two flex rows */}
+            <div>
+                {/* flex row 1, place and dates */}
+                <div className="flex flex-row justify-between">
+                    <div>Place</div>
+                    <div>{startDate} - {endDate}</div>
+                </div>
+
+                {/* flex row 2, title and dropdown button */}
+                <div className="flex flex-row justify-between">
+                    <div>{title}</div>
+                    <button onClick={() => setIsOpen(!isOpen)}>
+                        <svg className="-mr-1 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
+                            <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+
+            {/* dropdown TODO Make max height a little more or driven from height of page*/}
+            <div className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-48' : 'max-h-0'}`}>
+                experiences
+            </div>
+            
+        </div>
+    );
+};
--- src/components/sections/NrlSection.jsx
+++ src/components/sections/NrlSection.jsx
@@ -1,12 +1,13 @@
 import { NrlModel } from "src/components/models/NrlModel"
 import { InformationCard } from "src/components/InformationCard"
 import { Html } from "@react-three/drei"
+import * as THREE from "three"
 
 export const NrlSection = (props) => {
     return <>
         <NrlModel matcapManager={props.matcapManager} />
-        <Html as='div'>
-            <InformationCard />
+        <Html as='div' className="absolute w-80" position={new THREE.Vector3(0, 0, 0)}>
+            <InformationCard title={"nrl"} startDate={'20220101'} endDate={'20250102'} experiences={"here's some cool stuff"} />
         </Html>
     </>
 }
