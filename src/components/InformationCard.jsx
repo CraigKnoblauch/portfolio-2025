f3908118d847da787c391e80784f4a185448490c
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-02 19:21:56 -0500

    add merriweather font; information card looks good; will work on width stuff after the scroll logic is working

--- src/components/InformationCard.jsx
+++ src/components/InformationCard.jsx
@@ -8,23 +8,23 @@ import { useState } from 'react';
  *  - Dropdown content including a write up of experiences
  */
 
-export const InformationCard = ({ title, startDate, endDate, experiences }) => {
+export const InformationCard = ({ title, position, startDate, endDate, description }) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
         
-        <div className="bg-white opacity-75 p-4 rounded-lg">
+        <div className="merriweather-regular bg-white opacity-75 p-4 rounded-lg min-w-160">
             {/* header contains two flex rows */}
             <div>
                 {/* flex row 1, place and dates */}
                 <div className="flex flex-row justify-between">
-                    <div>Place</div>
-                    <div>{startDate} - {endDate}</div>
+                    <div className="merriweather-bold">{title}</div>
+                    <div className="merriweather-regular-italic">{startDate} - {endDate}</div>
                 </div>
 
                 {/* flex row 2, title and dropdown button */}
                 <div className="flex flex-row justify-between">
-                    <div>{title}</div>
+                    <div className="merriweather-regular-italic">{position}</div>
                     <button onClick={() => setIsOpen(!isOpen)}>
                         <svg className="-mr-1 size-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                             <path fillRule="evenodd" d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
@@ -34,8 +34,8 @@ export const InformationCard = ({ title, startDate, endDate, experiences }) => {
             </div>
 
             {/* dropdown TODO Make max height a little more or driven from height of page*/}
-            <div className={`overflow-hidden transition-all duration-500 ease-in-out ${isOpen ? 'max-h-48' : 'max-h-0'}`}>
-                experiences
+            <div className={`overflow-hidden transition-all duration-500 ease-in-out pt-2 ${isOpen ? 'max-h-48' : 'max-h-0'}`}>
+                {description}
             </div>
             
         </div>
--- src/components/sections/NrlSection.jsx
+++ src/components/sections/NrlSection.jsx
@@ -6,8 +6,13 @@ import * as THREE from "three"
 export const NrlSection = (props) => {
     return <>
         <NrlModel matcapManager={props.matcapManager} />
-        <Html as='div' className="absolute w-80" position={new THREE.Vector3(0, 0, 0)}>
-            <InformationCard title={"nrl"} startDate={'20220101'} endDate={'20250102'} experiences={"here's some cool stuff"} />
+        <Html as='div' className="relative" position={new THREE.Vector3(0, 0, 0)}>
+            <InformationCard 
+                title={"U.S. Naval Research Laboratory"} 
+                position={"Software Engineer in Spacecraft Engineering RDT&E"} 
+                startDate={'Jan. 2022'} endDate={'Present'} 
+                description={"Write software for the research of novel space technologies for national defense. Independently architect, develop, perform quality assurance, and effectively balance project and development needs through clear communication with management, stakeholders, and wider system teams."}
+            />
         </Html>
     </>
 }
