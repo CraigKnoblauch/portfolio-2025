5da3a93498e88ecf44a90954ddf442f524555bca
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-17 16:15:49 -0500

    text colors; intro title and paragraphs

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,12 @@ function App() {
         {/* Master flexbox container for the entire page. Gives column flex box with one column. That column has a max width */}
         <div className="flex flex-col items-center min-h-screen w-full"> 
             <div className="justify-left w-8/10">
-                <h1 className="text-[#efefef]">Craig Knoblauch</h1>
+                <h1 className="text-primary-white merriweather-bold text-lg">Craig Knoblauch</h1>
+                <div className="text-primary-white merriweather-regular text-sm space-y-2 pt-6">
+                    <p>This is a little about who I am</p>
+                    <p>This is a little about what I'm doing</p>
+                    <p>This is a little about what I've done</p>
+                </div>
             </div>
         </div>
     </>
