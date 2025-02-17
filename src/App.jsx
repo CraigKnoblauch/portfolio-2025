78012497b4e107359d28b6af61b646fc1ae18bd2
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-17 15:58:41 -0500

    massively simplifying portfolio; got good margins on elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { Canvas } from '@react-three/fiber'
-import { Experience } from 'src/components/Experience'
-import { ScrollControls } from '@react-three/drei'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Canvas>
-        <ScrollControls pages={8} damping={0.25}>
-            <Experience />
-        </ScrollControls>
-      </Canvas>
+        {/* Master flexbox container for the entire page. Gives column flex box with one column. That column has a max width */}
+        <div className="flex flex-col items-center min-h-screen w-full"> 
+            <div className="justify-left w-8/10">
+                <h1 className="text-[#efefef]">Craig Knoblauch</h1>
+            </div>
+        </div>
     </>
   )
 }
