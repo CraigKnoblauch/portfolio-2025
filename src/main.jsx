c5905fd527f6c3128e8900877f07b653ea618209
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-18 19:41:14 -0500

    this is how I get the whole content to go in the center of the screen on large displays

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Explorations from './Explorations.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Explorations />
+    <App />
+    {/* <Explorations /> */}
   </StrictMode>,
 )
