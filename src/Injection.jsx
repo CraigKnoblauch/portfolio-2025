9d1f2e112f3adda3f5d265b8ae206517e291541d
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-20 21:49:52 -0500

    prove that I can inject a three js component into markdown

--- src/Injection.jsx
+++ src/Injection.jsx
@@ -1,3 +1,23 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+
 export default function Injection() {
-    return <p>This is the injection</p>
+    const ref = useRef();
+    useFrame(() => {
+        if (ref.current) {
+            ref.current.rotation.y += 0.01;
+            ref.current.rotation.x += 0.01;
+        }
+    })
+
+    return (
+        <>
+            <ambientLight />
+            <perspectiveCamera position={[0, 0, 5]} />
+            <mesh ref={ref} rotation={[0, 0, 0]}>
+                <boxGeometry args={[1, 1, 1]} />
+                <meshNormalMaterial/>
+            </mesh>
+        </>
+    );
 }
