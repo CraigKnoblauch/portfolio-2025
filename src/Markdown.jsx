133e5322a511a601a3778aa16500dbd1e2b7a6de
Craig Knobaluch <craigknoblauch@gmail.com>
2025-02-22 12:06:30 -0500

    layout properties work for markdown as well

--- src/Markdown.jsx
+++ src/Markdown.jsx
@@ -4,9 +4,14 @@ function Markdown() {
 
   return (
     <>
-		<div className="prose merriweather-regular">
+		
+        <div className="flex flex-col items-center min-h-screen w-full"> 
+        <div className="justify-left w-8/10 lg:w-1/2">
+        <div className="prose merriweather-regular">
 			<Sample />
 		</div>
+        </div>
+        </div>
     </>
   )
 }
